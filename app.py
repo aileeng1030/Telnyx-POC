c2085c3191d3e72daca693c5100311834fd49b13
aileeng1030 <147579837+aileeng1030@users.noreply.github.com>
2023-10-12 21:58:41 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+import requests
+import sys 
+print(sys.path)
+
+app = Flask(__name__)
+
+# Set your Telnyx API credentials and phone numbers
+TELNYX_API_KEY = 'KEY018B0FF1A5CA289EBAC49057095C8E8C_ZivRGV6nzWCFocHnTY2ATi'
+TELNYX_PHONE_NUMBER = '+14016188903' # Your Telnyx phone number
+TELNYX_APP_PORT = '5000'
+# Function to send an MMS message
+def send_mms(to, body):
+    url = 'https://api.telnyx.com/v2/messages'
+    
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {TELNYX_API_KEY}'
+    }
+    
+    data = {
+        "from": TELNYX_PHONE_NUMBER,
+        "to": to,
+        "body": body
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        print("MMS sent successfully!")
+    else:
+        print(f"Error sending MMS. Status code: {response.status_code}")
+
+# Webhook endpoint to handle inbound messages
+@app.route('/webhook', methods=['POST'])
+def handle_inbound_message():
+    data = request.json
+    
+    # Extract relevant information from the inbound message
+    from_number = data['data']['payload']['from']
+    body = data['data']['payload']['body']
+    
+    # Process the inbound message
+    if "pizza" in body.lower():
+        response = "Chicago pizza is the best"
+    elif "ice cream" in body.lower():
+        response = "I prefer gelato"
+    else:
+        response = "please send word"
+    
+    # Send response as an MMS
+    send_mms(from_number, response)
+    
+    return jsonify(success=True)
+
+# Run the Flask application
+if __name__ == "__main__":
+    app.run(port=TELNYX_APP_PORT)
